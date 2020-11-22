e407398d6228ef7b7cff23b76534e57cf5174d19
Jerryzs <zhangs@winchesterthurston.org>
2020-11-22 17:50:57 +0800

    refactor(Global): create methods for making xhrs

--- src/global.js
+++ src/global.js
@@ -12,7 +12,45 @@ const GlobalObject = {
     auth: API + '/auth',
     user: API + '/user',
     userCheck: API + '/user/check'
-  }
+  },
+
+  request: (url, data = {}) => new Promise((resolve, reject) => {
+    url += '?' + Object.keys(data).map((key) => `${key}=${encodeURIComponent(data[key])}`).join('&');
+
+    const request = new window.XMLHttpRequest();
+
+    request.open('GET', url, true);
+    request.withCredentials = true;
+    request.responseType = 'json';
+
+    request.onload = () => {
+      const res = request.response;
+      if (!res.success) reject(res.message);
+      else resolve(res.data);
+    };
+    request.onerror = () => reject(request.status);
+
+    request.send();
+  }),
+
+  post: (url, data = {}) => new Promise((resolve, reject) => {
+    const raw = Object.keys(data).map((key) => `${key}=${encodeURIComponent(data[key])}`).join('&');
+
+    const request = new window.XMLHttpRequest();
+    request.open('POST', url, true);
+    request.withCredentials = true;
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.responseType = 'json';
+
+    request.onload = () => {
+      const res = request.response;
+      if (!res.success) reject(res.message);
+      else resolve(res.data);
+    };
+    request.onerror = () => reject(request.status);
+
+    request.send(raw);
+  })
 };
 
 export default GlobalObject;
