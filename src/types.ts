c599c3e4353cb0a311ebadc4064f8b1408e5f5aa
Jerryzs <zhangs@winchesterthurston.org>
2020-11-25 22:46:11 +0800

    refactor(types): make api response properties readonly

--- src/types.ts
+++ src/types.ts
@@ -4,47 +4,47 @@ declare global {
   const $0: typeof GlobalObject
 
   interface ResponseBase {
-    data: any
-    message: string
-    success: boolean
+    readonly data: any
+    readonly message: string
+    readonly success: boolean
   }
 
   interface UserBase {
-    lv: {
-      color: null | string
-      id: number
-      name: string
-      text_color: null | string
+    readonly lv: {
+      readonly color: null | string
+      readonly id: number
+      readonly name: string
+      readonly text_color: null | string
     }
-    name: string
-    picture: string
+    readonly name: string
+    readonly picture: string
   }
 
   interface User extends UserBase {
-    bio: string
-    code: number
-    email: string
-    exp: number
-    is_member: 0 | 1
-    is_moderator: 0 | 1
-    join_time: null | bigint
-    register_time: bigint
-    uid: number
-    verify: null | {
-      color: null | string
-      id: number
-      message: string
-      tag: string
-      text_color: null | string
+    readonly bio: string
+    readonly code: number
+    readonly email: string
+    readonly exp: number
+    readonly is_member: 0 | 1
+    readonly is_moderator: 0 | 1
+    readonly join_time: null | bigint
+    readonly register_time: bigint
+    readonly uid: number
+    readonly verify: null | {
+      readonly color: null | string
+      readonly id: number
+      readonly message: string
+      readonly tag: string
+      readonly text_color: null | string
     }
   }
 
   interface UserWithPage extends User {
-    user_page: string
+    readonly user_page: string
   }
 
   interface NoUser {
-    empty: true
+    readonly empty: true
   }
 
   type SessionUser = User | NoUser | undefined
